7aa13cb72251ca08c7ecdde281511a6effbaac8d
Hunter <61375197+JZXHanta@users.noreply.github.com>
2024-11-20 22:01:56 -0500

    Added simple http server to return days since and color as json

--- daysSince.py
+++ daysSince.py
@@ -1,5 +1,7 @@
 from datetime import datetime, date
 import argparse
+import uvicorn
+import server
 
 class COLORS:
     LIGHT_GREEN: str = "\033[1;32m"
@@ -16,19 +18,22 @@ def calculate(date_input: str) -> str:
     return str(days_difference)
 
 
-def get_date() -> str:
+def get_date():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "date",
+        "--date",
         help="date that you would like to calculate time since (Format: YYYY-MM-DD)"
+    )
+    parser.add_argument(
+        "--serve",
+        type=int,
+        help="port you would like to serve to"
     )
 
     args = parser.parse_args()
-    date = args.date
-    return date
+    return args.date, args.serve
 
-def colorify() -> str:
-    date_string: str = get_date()
+def colorify(date_string: str) -> str:
     days_since: str = calculate(date_string)
 
     if int(days_since) >= 30:
@@ -38,5 +43,9 @@ def colorify() -> str:
         return f"Days since {date_string}: {COLORS.LIGHT_GREEN}{days_since}{COLORS.END}"
 
 if __name__ == "__main__":
-    days_since: str = colorify()
-    print(days_since)
+    date_input, serve = get_date()
+    if serve:
+        uvicorn.run(server.app, host="0.0.0.0", port=serve)
+    else:
+        days_since: str = colorify(date_input)
+        print(days_since)
---
+++ server.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import daysSince
+
+app = FastAPI()
+
+class DaysSince(BaseModel):
+    days: int
+    color: str
+
+@app.get("/days_since/{date}")
+async def get_days_since(date: str):
+    days_since = daysSince.calculate(date)
+    color = "red" if int(days_since) >= 30 else "black"
+    return {"days": f"{days_since}", "color": f"{color}"}
